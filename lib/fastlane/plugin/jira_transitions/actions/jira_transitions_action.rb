bf600698f79f474d3eedea65a4172fe0e7c373b8
Ostap Taran <me@austinate.me>
2017-04-12 16:12:09 +0300

    Initial implementation

--- lib/fastlane/plugin/jira_transitions/actions/jira_transitions_action.rb
+++ lib/fastlane/plugin/jira_transitions/actions/jira_transitions_action.rb
@@ -1,8 +1,33 @@
 module Fastlane
   module Actions
+
+  require 'jira-ruby'
+
     class JiraTransitionsAction < Action
       def self.run(params)
-        UI.message("The jira_transitions plugin is working!")
+        Actions.verify_gem!('jira-ruby')
+
+        client = params[:client]
+        issue = client.Issue.find(params[:ticket_id])
+        issue_transitions = issue.transitions.all
+        transition_builder = issue.transitions.build
+
+        if params[:transition_id] 
+          transition_builder.save!('transition' => {'id' => params[:transition_id]})
+        else 
+          transition = issue_transitions.select { |transition|
+            if params[:matching]
+              params[:matching].call(transition)
+            else 
+              transition.name == params[:transition_name]
+            end
+          }.first
+          if !transition.nil? && !transition.id.nil?
+           transition_builder.save('transition' => {'id' => transition.id })
+          elsif params[:fail_if_no_matching] == true 
+            UI.crash!("0 matching transitions")
+          end
+        end
       end
 
       def self.description
@@ -24,19 +49,41 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "JIRA_TRANSITIONS_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :client,
+                                is_string: false,
+                                optional: false,
+                                type: JIRA::Client),
+          FastlaneCore::ConfigItem.new(key: :ticket_id,
+                               description: "ID of ticket",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :transition_id,
+                               description: "ID of transition to perform",
+                                  optional: true,
+                                      type: String,
+                              verify_block: proc do |value|
+                                              UI.user_error!("Invalid transition_id") if value.to_s.length == 0
+                                          end),
+          FastlaneCore::ConfigItem.new(key: :transition_name,
+                               description: "Name of transition to perform",
+                                  optional: true,
+                                      type: String,
+                              verify_block: proc do |value|
+                                              UI.user_error!("Invalid transition_name") if value.to_s.length == 0
+                                          end),
+          FastlaneCore::ConfigItem.new(key: :matching,
+                               description: "Block for custom transition matching options.
+                               Transition that is passed to block is 'transition' from jira-ruby gem",
+                                  optional: true,
+                                 is_string: false),
+          FastlaneCore::ConfigItem.new(key: :fail_if_no_matching,
+                               description: "Whether plugin should crash if there is no matching transition",
+                                  optional: true,
+                                 is_string: false)
         ]
       end
 
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://github.com/fastlane/fastlane/blob/master/fastlane/docs/Platforms.md
-        #
-        # [:ios, :mac, :android].include?(platform)
         true
       end
     end
